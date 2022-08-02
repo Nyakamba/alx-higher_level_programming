eaf682c267e1c0fd1b526696698e05d7ace13bf1
Nyakamba <omwegaenock@gmail.com>
2022-08-01 23:53:17 -0700

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 """
-Contains the "read_lines" function
+Contains the number_of_lines function
 """
 
 
-def read_lines(filename="", nb_lines=0):
-    """reads n lines of a text file (UTF8) and prints it to stdout"""
-    with open(filename, 'r', encoding='utf-8') as f:
-        if nb_lines <= 0:
-            print(f.read(), end='')
-            return
+def number_of_lines(filename=""):
+    """returns the number of lines of a text file"""
+    with open(filename, 'r', encoding='utf8') as f:
         i = 0
-        for i, line in enumerate(f):
-            if i == nb_lines:
-                break
-            print(line, end='')
+        for line in f:
+            i += 1
+        return i
